2cec33b423dc642ea61eec19c0a8ccde366df94c
ingfaraya <162543145+ingfaraya@users.noreply.github.com>
2024-03-24 22:53:11 -0300

    Grabación Sesión Semana 3 y aclaración Evaluación N1

---
+++ demo/src/main/java/demo/demo/Asignatura.java
@@ -0,0 +1,20 @@
+package demo.demo;
+
+public class Asignatura {
+
+    private String codigo;
+    private String nombre;
+
+    public Asignatura(String codigo, String nombre){
+        this.codigo = codigo;
+        this.nombre = nombre;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+}
--- demo/src/main/java/demo/demo/Student.java
+++ demo/src/main/java/demo/demo/Student.java
@@ -1,12 +1,23 @@
 package demo.demo;
 
+import java.util.List;
+import java.util.Map;
+
 public class Student {
+
     private int id;
     private String name;
+    private int age;
+    private List<Asignatura> asignaturas;
+    private Map<String, List<Double>> notasPorAsignatura;
 
-    public Student(int id, String name){
+    public Student(int id, String name, int age, List<Asignatura> asignaturas, Map<String, List<Double>> notasPorAsignatura) {
+
         this.id = id;
         this.name = name;
+        this.age = age;
+        this.asignaturas = asignaturas;
+        this.notasPorAsignatura = notasPorAsignatura;
     }
 
     public int getId() {
@@ -15,5 +26,17 @@ public class Student {
 
     public String getName() {
         return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public List<Asignatura> getAsignaturas(){
+        return asignaturas;
+    }
+
+    public Map<String, List<Double>> getNotasPorAsignatura(){
+        return notasPorAsignatura;
     }
 }
